4d8305015953604d547c56e479746ca3c5007460
lan ho <ithhlvn@gmail.com>
2024-08-20 15:29:03 +0700

    update1

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Lân Hồ.</p>
         <p>
-          Lân Hồ. Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
