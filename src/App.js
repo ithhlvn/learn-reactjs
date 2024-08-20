5d4d317ae7a20f52e67099164e2b48f7cc1b9fb3
lan ho <ithhlvn@gmail.com>
2024-08-20 16:37:50 +0700

    Update 2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "Hồ Hoàng Lân";
+  const age = 19;
+  const isMale = true;
+  const student = {
+    name: "Hồ Hoàng Lân",
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Lân Hồ.</p>
+
+        <p>Hello ReactJs.</p>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>
+          Xin chào: {name} - {age} {isMale ? "Male" : "Female"}{" "}
+        </p>
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+
+        {!isMale && <p>Female</p>}
+
         <a
           className="App-link"
           href="https://reactjs.org"
